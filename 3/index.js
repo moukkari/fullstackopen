4dba3524971b95b6f5b9acf7b80e608e3048f5c1
moukkari <ilmari.tyrkko@tuni.fi>
2020-08-26 13:19:01 +0300

    Tehtävät 3.1.-3.6.

---
+++ 3/index.js
@@ -0,0 +1,93 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+let persons = [
+    { 
+    "name": "Arto Hellas", 
+    "number": "040-123456",
+    "id": 1
+    },
+    { 
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523",
+    "id": 2
+    },
+    { 
+    "name": "Dan Abramov", 
+    "number": "12-43-234345",
+    "id": 3
+    },
+    { 
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122",
+    "id": 4
+    }
+]
+
+const getId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(p => p.id))
+        : 0
+    return maxId + 1
+}
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (req, res) => {
+    res.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+    `)
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const result = persons.find(person => person.id === id)
+    if (result) {
+        res.json(result)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        persons = persons.filter(p => p !== person) 
+        res.status(204).end()
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if (!body.name || !body.number) {
+        return res.status(400).json({error: 'content missing'})
+    } else if (persons.some(p => p.name === body.name)) {
+        return res.status(400).json({error: 'name is already in the book'})
+    }
+
+    const newPerson = {
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random() * Math.floor(100000)) //getId()
+    }
+
+    persons = persons.concat(newPerson)
+    res.json(newPerson)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
